06badf9aab21a3d159711bff85cde176b0d9ec83
Raghav <raghavawasthi2005@gmail.com>
2024-09-28 02:47:56 +0100

    can switch parties. TODO: click start quest (force click somehow)

--- app.py
+++ app.py
@@ -147,7 +147,16 @@ def get_iphone_mirroring_region():
         return None
 
 
-def action_text(general_fields, img_path, search_words, text_loc_word, harcoded_screen_percentages):
+def switch_party(region, party_num):
+    pag.moveTo(
+        (region[0] + region[2]) * (0.428 + (0.01575 * (party_num - 1))),
+        (region[1] + region[3]) * 0.255,
+    )
+    pag.click()
+
+
+def action_text(
+    general_fields, img_path, text_loc_word, harcoded_screen_percentages, search_words = []):
     region = general_fields["region"]
     left, top, width, height = general_fields["left"], general_fields["top"], general_fields["width"], general_fields["height"]
 
@@ -156,6 +165,24 @@ def action_text(general_fields, img_path, search_words, text_loc_word, harcoded_
         region={"top": top, "left": left, "width": width, "height": height},
         output_path=img_path,
     )
+
+    if search_words == []:
+        bbox = find_text_location(img_path, text_loc_word)
+        if bbox:
+            x, y, width, height = bbox
+            log(
+                f"Bounding box for '{text_loc_word}': x={x}, y={y}, width={width}, height={height}"
+            )
+            pag.moveTo(region[0] + x, region[1] + y)
+        else:
+            pag.moveTo(
+                (region[0] + region[2]) * harcoded_screen_percentages[0],
+                (region[1] + region[3]) * harcoded_screen_percentages[1],
+            )
+        pag.click()
+        time.sleep(GENERAL_LONG_SLEEP)
+
+        return
 
     search_word_results = [check_text_in_image(img_path, search_word) for search_word in search_words]
 
@@ -180,219 +207,231 @@ def main():
 
     region = get_iphone_mirroring_region()
     left, top, width, height = region
-
-    # click on search button
-    pag.moveTo((region[0] + region[2]) // 2, (region[1] + region[3]) * 0.85)
-    pag.click()
-
-    # open fgo
-    pag.write("fate/go", interval=0.05)
-    pag.press("enter")
-    log("fate/go entered into search bar.", "debug")
-
-    region = get_iphone_mirroring_region()
-    left, top, width, height = region
     general_fields = {"region": region, "left": left, "top": top, "width": width, "height": height}
 
-    # click for first loading screen
-    found = False
-    while not found:
-        capture_screenshot(
-            region={"top": top, "left": left, "width": width, "height": height},
-            output_path="img/screenshots/first_tap_on_open.png",
-        )
-        found = check_text_in_image(
-            "img/screenshots/first_tap_on_open.png", "Please Tap the Screen"
-        )
-        time.sleep(1)
-    pag.moveTo((region[0] + region[2]) // 2, (region[1] + region[3]) // 2)
-    pag.click()
+    # # click on search button
+    # pag.moveTo((region[0] + region[2]) // 2, (region[1] + region[3]) * 0.85)
+    # pag.click()
 
-    # click for second loading screen
-    time.sleep(GENERAL_LONG_SLEEP)
-    pag.click()
-    time.sleep(GENERAL_LONG_SLEEP)
+    # # open fgo
+    # pag.write("fate/go", interval=0.05)
+    # pag.press("enter")
+    # log("fate/go entered into search bar.", "debug")
 
-    # in case friend popup comes up
-    action_text(general_fields, "img/screenshots/potential_friend_popup.png", ["Friend Points", "Most used Servant"], "Close", [0.3, 0.85])
-    
-    # in case news shows up
-    capture_screenshot(
-        region={"top": top, "left": left, "width": width, "height": height},
-        output_path="img/screenshots/potential_news.png",
-    )
-    search_words = ["News", "Maintenance", "Issues", "Facebook", "X (Twitter)"]
-    search_words_results = [check_text_in_image("img/screenshots/potential_news.png", search_word) for search_word in search_words]
-    if True in search_words_results:
-        pag.moveTo((region[0] + region[2]) * 0.885, (region[1] + region[3]) * 0.24)
-        pag.click()
-        time.sleep(GENERAL_LONG_SLEEP)
+    # region = get_iphone_mirroring_region()
+    # left, top, width, height = region
+    # general_fields = {"region": region, "left": left, "top": top, "width": width, "height": height}
 
-    # in case of any close buttons
-    capture_screenshot(
-        region={"top": top, "left": left, "width": width, "height": height},
-        output_path="img/screenshots/potential_close_btns.png",
-    )
-    loc_close_happened = False
-   
+    # # click for first loading screen
+    # found = False
+    # while not found:
+    #     capture_screenshot(
+    #         region={"top": top, "left": left, "width": width, "height": height},
+    #         output_path="img/screenshots/first_tap_on_open.png",
+    #     )
+    #     found = check_text_in_image(
+    #         "img/screenshots/first_tap_on_open.png", "Please Tap the Screen"
+    #     )
+    #     time.sleep(1)
+    # pag.moveTo((region[0] + region[2]) // 2, (region[1] + region[3]) // 2)
+    # pag.click()
+
+    # # click for second loading screen
+    # time.sleep(GENERAL_LONG_SLEEP)
+    # pag.click()
+    # time.sleep(GENERAL_LONG_SLEEP)
+
+    # # in case friend popup comes up
+    # action_text(general_fields, "img/screenshots/potential_friend_popup.png", "Close", [0.3, 0.85], ["Friend Points", "Most used Servant"])
+
+    # # in case news shows up
+    # capture_screenshot(
+    #     region={"top": top, "left": left, "width": width, "height": height},
+    #     output_path="img/screenshots/potential_news.png",
+    # )
+    # search_words = ["News", "Maintenance", "Issues", "Facebook", "X (Twitter)"]
+    # search_words_results = [check_text_in_image("img/screenshots/potential_news.png", search_word) for search_word in search_words]
+    # if True in search_words_results:
+    #     pag.moveTo((region[0] + region[2]) * 0.885, (region[1] + region[3]) * 0.24)
+    #     pag.click()
+    #     time.sleep(GENERAL_LONG_SLEEP)
+
+    # # in case of any close buttons
+    # capture_screenshot(
+    #     region={"top": top, "left": left, "width": width, "height": height},
+    #     output_path="img/screenshots/potential_close_btns.png",
+    # )
+    # loc_close_happened = False
+
+    # try:
+    #     loc_close = pag.locateCenterOnScreen("img/screenshots/close_btn.png", confidence=0.8)
+    #     loc_close_exists = True
+    # except pag.ImageNotFoundException:
+    #     loc_close_exists = False
+
+    # while loc_close_exists or check_text_in_image("img/screenshots/potential_close_btns.png", "Close"):
+    #     loc_close_happened = True
+    #     if loc_close_exists:
+    #         pag.moveTo(loc_close.x, loc_close.y)
+    #         pag.click()
+    #     else:
+    #         bbox = find_text_location(
+    #             "img/screenshots/potential_close_btns.png", "Close"
+    #         )
+    #         if bbox:
+    #             x, y, width, height = bbox
+    #             log(
+    #                 f"Bounding box for 'Close': x={x}, y={y}, width={width}, height={height}"
+    #             )
+    #             pag.moveTo(region[0] + x, region[1] + y)
+    #         else:
+    #             pag.moveTo((region[0] + region[2]) * 0.5, (region[1] + region[3]) * 0.835)
+    #         pag.click()
+
+    #     try:
+    #         loc_close = pag.locateCenterOnScreen("img/screenshots/close_btn.png", confidence=0.8)
+    #         loc_close_exists = True
+    #     except pag.ImageNotFoundException:
+    #         loc_close_exists = False
+
+    # if loc_close_happened:
+    #     time.sleep(GENERAL_LONG_SLEEP)
+
+    # # drag scroll bar to top of screen
+    # pag.moveTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.35)
+    # pag.click()
+    # pag.dragTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.3, button="left")
+
+    # # open Chaldea Gate menu
+    # try:
+    #     loc_chaldea_gate = pag.locateCenterOnScreen("img/screenshots/chaldea_gate.png", confidence=0.8)
+    #     pag.moveTo(loc_chaldea_gate.x // 2, loc_chaldea_gate.y // 2)
+    #     pag.click()
+    # except pag.ImageNotFoundException:
+    #     capture_screenshot(
+    #         region={"top": top, "left": left, "width": width, "height": height},
+    #         output_path="img/screenshots/fgo_in_game_homescreen.png",
+    #     )
+    #     bbox = find_text_location(
+    #         "img/screenshots/fgo_in_game_homescreen.png", "Chaldea Gate"
+    #     )
+    #     if bbox:
+    #         x, y, width, height = bbox
+    #         log(
+    #             f"Bounding box for 'Chaldea Gate': x={x}, y={y}, width={width}, height={height}"
+    #         )
+    #         pag.moveTo(region[0] + x, region[1] + y)
+    #     else:
+    #         pag.moveTo((region[0] + region[2]) * 0.7, (region[1] + region[3]) * 0.61) # NOTE: won't work if event - change 0.61 to something higher
+    #     pag.click()
+
+    # # open Daily Quests menu
+    # capture_screenshot(
+    #     region={"top": top, "left": left, "width": width, "height": height},
+    #     output_path="img/screenshots/chaldea_gate_menu.png",
+    # )
+    # bbox = find_text_location("img/screenshots/chaldea_gate_menu.png", "Daily Quests")
+    # if bbox:
+    #     x, y, width, height = bbox
+    #     log(
+    #         f"Bounding box for 'Daily Quests': x={x}, y={y}, width={width}, height={height}"
+    #     )
+    #     pag.moveTo(region[0] + x, region[1] + y)
+    # else:
+    #     pag.moveTo((region[0] + region[2]) * 0.7, (region[1] + region[3]) * 0.4)
+    # pag.click()
+
+    # # drag scroll bar to top of screen
+    # pag.moveTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.35)
+    # pag.click()
+    # pag.dragTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.3, button="left")
+
+    # # scroll to find Extreme QP quest
+    # curr_y = (region[1] + region[3]) * 0.3
+    # capture_screenshot(
+    #     region={"top": top, "left": left, "width": width, "height": height},
+    #     output_path="img/screenshots/daily_quests_menu.png",
+    # )
+    # bbox = find_text_location(
+    #     "img/screenshots/daily_quests_menu.png", "Enter the Treasure Vault - Extreme"
+    # )
+    # if bbox:
+    #     x, y, width, height = bbox
+    #     log(
+    #         f"Bounding box for 'Enter the Treasure Valut - Extreme': x={x}, y={y}, width={width}, height={height}"
+    #     )
+    #     pag.moveTo(region[0] + x, region[1] + y)
+    # else:
+    #     while not bbox:
+    #         if curr_y >= ((region[1] + region[3]) * 0.835):
+    #             break
+
+    #         curr_y += (region[1] + region[3]) * 0.05
+    #         pag.dragTo((region[0] + region[2]) * 0.925, curr_y, button="left")
+
+    #         capture_screenshot(
+    #             region={"top": top, "left": left, "width": width, "height": height},
+    #             output_path="img/screenshots/daily_quests_menu.png",
+    #         )
+    #         bbox = find_text_location(
+    #             "img/screenshots/daily_quests_menu.png", "Enter the Treasure Vault - Extreme"
+    #         )
+
+    #     if bbox:
+    #         x, y, width, height = bbox
+    #         log(
+    #             f"Bounding box for 'Enter the Treasure Vault - Extreme': x={x}, y={y}, width={width}, height={height}"
+    #         )
+    #         pag.moveTo(region[0] + x, region[1] + y)
+    #     else:
+    #         pag.moveTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.835)
+    #         pag.click()
+
+    #         # click on QP Extreme quest
+    #         pag.moveTo((region[0] + region[2]) * 0.7, (region[1] + region[3]) * 0.4)
+    #         pag.click()
+
+    # time.sleep(GENERAL_LONG_SLEEP)
+
+    # # switch to caster class
+    # loc = pag.locateCenterOnScreen("img/screenshots/friend_support_caster_class.png", confidence=0.8)
+    # pag.moveTo(loc.x // 2, loc.y // 2)
+    # pag.click()
+
+    # # make sure at top of list
+    # pag.moveTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.4)
+    # pag.click()
+
+    # # find end of scrollbar if need to scroll
+    # loc = pag.locateOnScreen("img/screenshots/friend_support_scrollbar.png", confidence=0.8)
+    # curr_y = (loc.top + loc.height) // 2
+    # pag.moveTo((loc.left + (loc.width / 2)) // 2, curr_y)
+
+    # found = False
+    # while not found:
+    #     try:
+    #         # select castoria
+    #         loc_support = pag.locateCenterOnScreen("img/screenshots/friend_support_altria_caster.png", confidence=0.8)
+    #         pag.moveTo(loc_support.x // 2, loc_support.y // 2)
+    #         pag.click()
+    #         found = True
+    #     except pag.ImageNotFoundException:
+    #         # scroll to next supports
+    #         curr_y += loc.height // 4
+    #         pag.dragTo((region[0] + region[2]) * 0.925, curr_y, button="left", duration=0.5)
+
+    # switch to party VIII - QP farming party
+    switch_party(region, 8)
+
+    # start quest
     try:
-        loc_close = pag.locateCenterOnScreen("img/screenshots/close_btn.png", confidence=0.8)
-        loc_close_exists = True
+        loc_start_quest = pag.locateCenterOnScreen("img/screenshots/start_quest.png", confidence=0.8)
+        pag.moveTo(loc_start_quest.x // 2, loc_start_quest.y // 2)
+
+        pag.click(button="right")
+
     except pag.ImageNotFoundException:
-        loc_close_exists = False
-
-    while loc_close_exists or check_text_in_image("img/screenshots/potential_close_btns.png", "Close"):
-        loc_close_happened = True
-        if loc_close_exists:
-            pag.moveTo(loc_close.x, loc_close.y)
-            pag.click()
-        else:
-            bbox = find_text_location(
-                "img/screenshots/potential_close_btns.png", "Close"
-            )
-            if bbox:
-                x, y, width, height = bbox
-                log(
-                    f"Bounding box for 'Close': x={x}, y={y}, width={width}, height={height}"
-                )
-                pag.moveTo(region[0] + x, region[1] + y)
-            else:
-                pag.moveTo((region[0] + region[2]) * 0.5, (region[1] + region[3]) * 0.835)
-            pag.click()
-
-        try:
-            loc_close = pag.locateCenterOnScreen("img/screenshots/close_btn.png", confidence=0.8)
-            loc_close_exists = True
-        except pag.ImageNotFoundException:
-            loc_close_exists = False
-
-    if loc_close_happened:
-        time.sleep(GENERAL_LONG_SLEEP)
-
-    # drag scroll bar to top of screen
-    pag.moveTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.35)
-    pag.click()
-    pag.dragTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.3, button="left")
-    
-    # open Chaldea Gate menu
-    try:
-        loc_chaldea_gate = pag.locateCenterOnScreen("img/screenshots/chaldea_gate.png", confidence=0.8)
-        pag.moveTo(loc_chaldea_gate.x // 2, loc_chaldea_gate.y // 2)
-        pag.click()
-    except pag.ImageNotFoundException:
-        capture_screenshot(
-            region={"top": top, "left": left, "width": width, "height": height},
-            output_path="img/screenshots/fgo_in_game_homescreen.png",
-        )
-        bbox = find_text_location(
-            "img/screenshots/fgo_in_game_homescreen.png", "Chaldea Gate"
-        )
-        if bbox:
-            x, y, width, height = bbox
-            log(
-                f"Bounding box for 'Chaldea Gate': x={x}, y={y}, width={width}, height={height}"
-            )
-            pag.moveTo(region[0] + x, region[1] + y)
-        else:
-            pag.moveTo((region[0] + region[2]) * 0.7, (region[1] + region[3]) * 0.61) # NOTE: won't work if event - change 0.61 to something higher
-        pag.click()
-    
-    # open Daily Quests menu
-    capture_screenshot(
-        region={"top": top, "left": left, "width": width, "height": height},
-        output_path="img/screenshots/chaldea_gate_menu.png",
-    )
-    bbox = find_text_location("img/screenshots/chaldea_gate_menu.png", "Daily Quests")
-    if bbox:
-        x, y, width, height = bbox
-        log(
-            f"Bounding box for 'Daily Quests': x={x}, y={y}, width={width}, height={height}"
-        )
-        pag.moveTo(region[0] + x, region[1] + y)
-    else:
-        pag.moveTo((region[0] + region[2]) * 0.7, (region[1] + region[3]) * 0.4)
-    pag.click()
-
-    # drag scroll bar to top of screen
-    pag.moveTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.35)
-    pag.click()
-    pag.dragTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.3, button="left")
-    
-    # scroll to find Extreme QP quest
-    curr_y = (region[1] + region[3]) * 0.3
-    capture_screenshot(
-        region={"top": top, "left": left, "width": width, "height": height},
-        output_path="img/screenshots/daily_quests_menu.png",
-    )
-    bbox = find_text_location(
-        "img/screenshots/daily_quests_menu.png", "Enter the Treasure Vault - Extreme"
-    )
-    if bbox:
-        x, y, width, height = bbox
-        log(
-            f"Bounding box for 'Enter the Treasure Valut - Extreme': x={x}, y={y}, width={width}, height={height}"
-        )
-        pag.moveTo(region[0] + x, region[1] + y)
-    else:
-        while not bbox:
-            if curr_y >= ((region[1] + region[3]) * 0.835):
-                break
-
-            curr_y += (region[1] + region[3]) * 0.05
-            pag.dragTo((region[0] + region[2]) * 0.925, curr_y, button="left")
-            
-            capture_screenshot(
-                region={"top": top, "left": left, "width": width, "height": height},
-                output_path="img/screenshots/daily_quests_menu.png",
-            )
-            bbox = find_text_location(
-                "img/screenshots/daily_quests_menu.png", "Enter the Treasure Vault - Extreme"
-            )
-
-        if bbox:
-            x, y, width, height = bbox
-            log(
-                f"Bounding box for 'Enter the Treasure Vault - Extreme': x={x}, y={y}, width={width}, height={height}"
-            )
-            pag.moveTo(region[0] + x, region[1] + y)
-        else:
-            pag.moveTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.835)
-            pag.click()
-
-            # click on QP Extreme quest
-            pag.moveTo((region[0] + region[2]) * 0.7, (region[1] + region[3]) * 0.4)
-            pag.click()
-
-    time.sleep(GENERAL_LONG_SLEEP)
-
-    # switch to caster class
-    loc = pag.locateCenterOnScreen("img/screenshots/friend_support_caster_class.png", confidence=0.8)
-    pag.moveTo(loc.x // 2, loc.y // 2)
-    pag.click()
-
-    # make sure at top of list
-    pag.moveTo((region[0] + region[2]) * 0.925, (region[1] + region[3]) * 0.4)
-    pag.click()
-
-    # find end of scrollbar if need to scroll
-    loc = pag.locateOnScreen("img/screenshots/friend_support_scrollbar.png", confidence=0.8)
-    curr_y = (loc.top + loc.height) // 2
-    pag.moveTo((loc.left + (loc.width / 2)) // 2, curr_y)
-
-    found = False
-    while not found:
-        try:
-            # select castoria
-            loc_support = pag.locateCenterOnScreen("img/screenshots/friend_support_altria_caster.png", confidence=0.8)
-            pag.moveTo(loc_support.x // 2, loc_support.y // 2)
-            pag.click()
-            found = True
-        except pag.ImageNotFoundException:
-            # scroll to next supports
-            curr_y += loc.height // 4
-            pag.dragTo((region[0] + region[2]) * 0.925, curr_y, button="left", duration=0.5)
-
-    # wait for game to load and then pop correct skills (maybe image recognition for when new wave?)
+        action_text(general_fields, "img/screenshots/start_quest.png", "Start Quest", [0.88, 0.9])
 
 
 if __name__ == "__main__":
